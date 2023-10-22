9fbeb4671b3a22f000e3753f079864dcb5a83ee7
musteve <mu23818@g.ecc.u-tokyo.ac.jp>
2023-10-22 23:55:31 +0900

    add

--- main.go
+++ main.go
@@ -1,21 +1,89 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type responseMessage struct {
-	Message string `json:"message"`
+
+var db *sql.DB
+
+func init() {
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPwd := os.Getenv("MYSQL_PWD")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+
+	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	_db, err := sql.Open("mysql", connStr)
+	if err != nil {
+		log.Fatalf("fail: sql.Open, err")
+	}
+	if err := _db.Ping(); err != nil {
+		log.Fatalf("fail: _db.Ping, err")
+	}
+	db = _db
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+
+type user struct {
+	Id 	string `json:"id"`
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "https://hackathon-frontend-o35b.vercel.app")
+	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CITE_VERCEL"))
+
+	rows, err := db.Query("select * from user")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	users := make([]user, 0)
+	for rows.Next() {
+		var u user
+		if err := rows.Scan(&u.Id, &u.Name, &u.Age); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		users = append(users, u)
+	}
+
+	bytes, err := json.Marshal(users)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(bytes)
+
+}
+
+
+type responseMessage struct {
+	Message string `json:"message"`
+}
+
+func handlerHelloWorld(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CITE_VERCEL"))
 
 	bytes, err := json.Marshal(responseMessage{
 		Message: "Hello, World!",
@@ -28,6 +96,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/hello", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/hello", handlerHelloWorld)
+	http.HandleFunc("/getusers", getUsers)
+	http.ListenAndServe(":8080", nil)
 }
